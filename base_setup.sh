b3bc0a0dbc617f843fc258d769675f0ee233ce13
Tyler Peterson <tylermpeterson36@gmail.com>
2021-01-20 22:58:46 -0800

    minor tweaks

--- base_setup.sh
+++
@@ -1,1 +0,0 @@
-sudo ./examples-api-use/demo --led-rows 32 --led-cols 64 --led-gpio-mapping=adafruit-hat  --led-show-refresh --led-slowdown-gpio=4 -D7 -m 30
--- bindings/python/create_app.py
+++ bindings/python/create_app.py
@@ -1,24 +1,32 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import Union
 import os
 
 
-class ModeSelection(BaseModel):
+class FileSelection(BaseModel):
     python_file: str
 
 
+class ImageScroller(FileSelection):
+    python_file: str = "song_scroller2"
+    image_path: str
+    artist: str
+    title: str
+
+
 def get_script_args_for_mode(mode: str) -> str:
-    if mode == "song_scroller2":
+    if isinstance(mode, ImageScroller):
         return (
             " -i "
-            + "./custom_displays/bigwild.jpg"
+            + f"./custom_displays/{mode.image_path}"
             + " -a "
-            + f"'Big Wild'"
+            + f"'{mode.artist}'"
             + " -s "
-            + f"'when I get there'"
+            + f"'{mode.title}'"
         )
 
-
+    return ""
 
 
 def create_app():
@@ -26,11 +34,11 @@ def create_app():
     app = FastAPI()
 
     @app.post("/display")
-    async def set_display(mode: ModeSelection):
+    async def set_display(mode: Union[FileSelection, ImageScroller]):
         base_script = str(
             f"sudo python3 custom_displays/{mode.python_file}.py --led-rows 32 --led-cols 64 --led-gpio-mapping=adafruit-hat --led-show-refresh --led-slowdown-gpio=3 "
         )
-        script_args = get_script_args_for_mode(mode.python_file)
+        script_args = get_script_args_for_mode(mode)
         script = base_script + " " + script_args
         print("executing")
         print(script)
